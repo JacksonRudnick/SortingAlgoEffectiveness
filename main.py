bb8ecdfb7d788620b3165ebc01b777b21b009d0a
Jackson <JacksonRudnick32@gmail.com>
2024-02-22 10:29:35 -0600

    Upgrades people upgrades

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import numpy as np
+import timeit
+
 # Bubble Sort Implementation
 # Resource: https://www.geeksforgeeks.org/bubble-sort/
 def bubbleSort(arr):
@@ -69,70 +72,68 @@ def mergeSort(arr):
 # Quick Sort Implementation
 # Resource: https://www.geeksforgeeks.org/quick-sort/
 def partition(array, low, high):
- 
     # Choose the rightmost element as pivot
     pivot = array[high]
- 
+
     # Pointer for greater element
     i = low - 1
- 
+
     # Traverse through all elements
     # compare each element with pivot
     for j in range(low, high):
         if array[j] <= pivot:
- 
+
             # If element smaller than pivot is found
             # swap it with the greater element pointed by i
             i = i + 1
- 
+
             # Swapping element at i with element at j
             (array[i], array[j]) = (array[j], array[i])
- 
+
     # Swap the pivot element with
     # the greater element specified by i
     (array[i + 1], array[high]) = (array[high], array[i + 1])
- 
+
     # Return the position from where partition is done
     return i + 1
- 
- 
+
+
 # Function to perform quicksort
 def quicksort(array, low, high):
     if low < high:
- 
+
         # Find pivot element such that
         # element smaller than pivot are on the left
         # element greater than pivot are on the right
         pi = partition(array, low, high)
- 
+
         # Recursive call on the left of pivot
         quicksort(array, low, pi - 1)
- 
+
         # Recursive call on the right of pivot
         quicksort(array, pi + 1, high)
 
 # Radix Sort Implementation
 # Resource: https://www.geeksforgeeks.org/radix-sort/
 def countingSort(arr, exp1):
- 
     n = len(arr)
- 
+
     # The output array elements that will have sorted arr
     output = [0] * (n)
- 
+
     # initialize count array as 0
     count = [0] * (10)
- 
+
     # Store count of occurrences in count[]
     for i in range(0, n):
         index = arr[i] // exp1
         count[index % 10] += 1
- 
+
     # Change count[i] so that count[i] now contains actual
     # position of this digit in output array
     for i in range(1, 10):
         count[i] += count[i - 1]
- 
+
     # Build the output array
     i = n - 1
     while i >= 0:
@@ -140,21 +141,19 @@ def countingSort(arr, exp1):
         output[count[index % 10] - 1] = arr[i]
         count[index % 10] -= 1
         i -= 1
- 
+
     # Copying the output array to arr[],
     # so that arr now contains sorted numbers
     i = 0
     for i in range(0, len(arr)):
         arr[i] = output[i]
- 
+
 # Method to do Radix Sort
- 
- 
 def radixSort(arr):
- 
+
     # Find the maximum number to know number of digits
     max1 = max(arr)
- 
+
     # Do counting sort for every digit. Note that instead
     # of passing digit number, exp is passed. exp is 10^i
     # where i is current digit number
@@ -163,6 +162,17 @@ def radixSort(arr):
         countingSort(arr, exp)
         exp *= 10
 
+def doTest(scenario, func):
+    arr[100000] = {None}
+
+    for x in range(100):
+        arr[x] = np.random.randint(1,100000)
+
+    
+    return timeit.timeit(func(arr))
+
+
+
 #############
 # Main Code #
 #############
@@ -170,9 +180,23 @@ def radixSort(arr):
 if __name__ == "__main__":
     print("Welcome to the test suite of selected sorting algorithms!")
 
-    input("Select the sorting algorithm you want to test.\n-------------------------\n1. Bubble Sort\n2. Merge Sort\n3. Quick Sort\n4. Radix Sort\n5. Exit\n\nSelect a sorting algorithm (1-5): ")
+    quit = 0
+    scenario = 0
 
-    input("Case Scenarios for Bubble Sort\n---------------\n1. Best Case\n2. Average Case\n3. Worst Case\n4. Exit bubble sort test\n\nSelect the case (1-4): ")
+    while quit != 5:
+        quit = int(input("Select the sorting algorithm you want to test.\n-------------------------\n1. Bubble Sort\n2. Merge Sort\n3. Quick Sort\n4. Radix Sort\n5. Exit\n\nSelect a sorting algorithm (1-5): "))
+
+        match quit:
+            case 1:
+                scenario = int(input("Case Scenarios for Bubble Sort\n---------------\n1. Best Case\n2. Average Case\n3. Worst Case\n4. Exit bubble sort test\n\nSelect the case (1-4): "))
+                print(doTest(scenario, bubbleSort({0})))
+
+            case 2:
+                scenario = input("Case Scenarios for Merge Sort\n---------------\n1. Best Case\n2. Average Case\n3. Worst Case\n4. Exit merge sort test\nSelect the case (1-4): ")
+            case 3:
+                scenario = input("Case Scenarios for Quick Sort\n---------------\n1. Best Case\n2. Average Case\n3. Worst Case\n4. Exit quick sort test\nSelect the case (1-4): ")
+            case 4:
+                scenario = input("Case Scenarios for Radix Sort\n---------------\n1. Best Case\n2. Average Case\n3. Worst Case\n4. Exit radix sort test\nSelect the case (1-4): ")
 
 
 
@@ -180,3 +204,5 @@ if __name__ == "__main__":
 
 
 
+
+    print("Bye!")
